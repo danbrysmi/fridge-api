68eda278e960e2c0838055788a905e81d631e26c
Dan Bryan-Smith <danbrysmi@gmail.com>
2022-07-16 00:52:29 +0100

    Added item details page

--- fridge-api/src/main/java/com/example/fridgeapi/FridgeController.java
+++ fridge-api/src/main/java/com/example/fridgeapi/FridgeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,10 +26,25 @@ public class FridgeController {
 		return "food/index";
 	}
 	
-	@GetMapping(value = "/add")
+	@GetMapping(value="/add")
 	public String newEvent(Model model) {
 		model.addAttribute("item", new FridgeItem());
 		return "food/add";
+	}
+	
+	@GetMapping(value="/{id}")
+	public String getEventById(@PathVariable("id") long id, Model model) {
+		FridgeItem selectedItem = fridgeRepo.findById(id);
+		
+		if (selectedItem == null) {
+			selectedItem = new FridgeItem("empty", "empty", 404);
+		}
+		model.addAttribute("itemId", id);
+		model.addAttribute("itemName", selectedItem.getName());
+		model.addAttribute("itemFoodType", selectedItem.getFoodType());
+		model.addAttribute("itemQuantity", selectedItem.getQuantity());
+		
+		return "food/item";
 	}
 	
 	@PostMapping
